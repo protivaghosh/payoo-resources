30f81e3ea2286e63dee9867941c99a935b3f7aa9
protivaghosh <protivaghosh50@gmail.com>
2024-09-19 11:04:45 +0600

    added add money to event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
-console.log('home added');
+// add money added
+// step-1:add an even handler to the added money button inside the from
+document.getElementById('button-add-money').addEventListener('click', function(event){
+    //  prevent page load after from submit
+    event.preventDefault();
+    // console.log('money add button click')
+    // step-2:get money to be added to the account
+    const amountNumber = document.getElementById('amount-number').value;
+    const digitNumber =document.getElementById('digit-number').value;
+    console.log(amountNumber, digitNumber);
+})
