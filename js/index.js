656a501979c06ea7d1cd2f59901719381eb67595
protivaghosh <protivaghosh50@gmail.com>
2024-09-19 08:59:32 +0600

    added to payoo home page

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
-console.log('ami bari jai');
 // login button
 // step-1:set event handler
 document.getElementById('button-login').addEventListener('click', function(event){
 //    step-2:prevent default behavior(prevent reloading browser) 
     event.preventDefault();
-    console.log('login button clicked');
-    // step-3:get the phone number
-    const phoneNumber = document.getElementById('phone-number');
-    console.log(phoneNumber.value);
+   
+    // step-3:get the phone number and the pin number
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber =document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+    // step-4:validate pin and phone
+// this is temporary.you should like this
+    if(phoneNumber === '5' && pinNumber ==='1234'){
+        console.log('you are login');
+    }
+    // step-5:allow user to use the website
+    else{
+        alert('wrong phn number or pin');
+    }
+    
 })
